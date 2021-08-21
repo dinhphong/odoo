70692638f04abfc2d3cb63435ee9137a35d37d5b
dinhphong <dinhphong72@gmail.com>
2021-08-21 13:13:21 +0700

    Remove 2 testing addons

--- addons/intro_to_testing/models/demo_access_right.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields, api
-
-
-class DemoAccessRight(models.Model):
-    _name = 'demo.access.right'
-    _description = 'Demo Access Right Model'
-
-    name = fields.Char(string='Name', required=True)
